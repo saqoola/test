bda67b5ab49109fb653a1927284cc19fb1dd7f04
saqoola <103890754+saqoola@users.noreply.github.com>
2022-04-18 20:59:13 +0900

    Add files via upload

---
+++ 1/1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  1
+//
+//  Created by 111 on 2022/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .foregroundColor(Color.red)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1/1/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  1
+//
+//  Created by 111 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
