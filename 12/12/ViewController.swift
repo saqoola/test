429205c8b330cab97b4133e2bef513655da21696
saqoola <103890754+saqoola@users.noreply.github.com>
2022-04-18 20:55:12 +0900

    Add files via upload

---
+++ 12/12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12
+//
+//  Created by 111 on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
